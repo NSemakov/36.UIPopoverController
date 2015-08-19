e1bfe9d85eab67e380eda0c9697ef3b506caddcc
Nikita Semakov <n.semakov@yahoo.com>
2015-08-19 03:54:34 +0300

    Initial Commit

---
+++ 36. UIPopover/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  36. UIPopover
+//
+//  Created by Admin on 19.08.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
